61368fc6ef3ce88f6cec6cfb8516551c89904779
Nikolai Maslak <nikolai.maslak@brightpattern.com>
2023-10-17 04:25:02 +0800

    mysql

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
+import { createConnection } from "mysql2/promise";
+
+const connection = await createConnection({
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    port: Number(process.env.MYSQLPORT),
+    database: process.env.MYSQLDATABASE,
+});
+
 Bun.serve({
     port: process.env.PORT || 3001,
-    fetch(request) {
-        return new Response("Hello from Bun!");
+    async fetch(request) {
+        const [rows] = await connection.execute("SELECT 1+2 AS count");
+        return new Response("Hello from Bun!" + JSON.stringify(rows));
     },
 });
